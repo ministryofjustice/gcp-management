5e307d333d958386ada2bd6ec561c8416762e463
Jacob Woffenden <jacob@woffenden.io>
2026-08-13 14:39:13 +0100

    ❇️ Add initial files (#5)
    * Add initial files
    
    Signed-off-by: Jacob Woffenden <jacob.woffenden@justice.gov.uk>
    
    * Update CODEOWNERS
    
    Signed-off-by: Jacob Woffenden <jacob.woffenden@justice.gov.uk>
    
    ---------
    
    Signed-off-by: Jacob Woffenden <jacob.woffenden@justice.gov.uk>
    Signed-off-by: Jacob Woffenden <jacob@woffenden.io>

---
+++ .devcontainer/post-create.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+
+set -euo pipefail
+
+# Install pre-commit hooks
+uvx pre-commit install
+
+# Install apm dependencies
+apm install --frozen
+
+# Install Google Cloud CLI
+curl https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo gpg --dearmor -o /usr/share/keyrings/cloud.google.gpg
+
+echo "deb [signed-by=/usr/share/keyrings/cloud.google.gpg] https://packages.cloud.google.com/apt cloud-sdk main" | sudo tee -a /etc/apt/sources.list.d/google-cloud-sdk.list
+
+sudo apt-get update && sudo apt-get install google-cloud-cli
---
+++ .github/instructions/commit.instructions.md
@@ -0,0 +1,8 @@
+---
+description: Commit Style Instructions
+---
+
+# Commit Styling
+
+- Use [Conventional Commits](https://www.conventionalcommits.org)
+- Make sure commits are signed; this is a common requirement in our repositories
